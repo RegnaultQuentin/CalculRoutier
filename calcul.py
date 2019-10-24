c3bd007e203731c7d879e44f1fd27b9ae4a7da1a
Quentin Regnault <quentin.regnault@ynov.com>
2019-10-24 10:21:34 +0200

    update

--- calcul.py
+++ calcul.py
@@ -1,23 +1,32 @@
+from time import *
 import os
 import csv
 import requests
 from bs4 import BeautifulSoup
 
-departville = input('Choisir la ville de départ\n')
-arriveville = input("Choisir la ville d'arriver\n")
-
 
 
 def tempstrajet() :
+
+
+    # Récupération des villes
+    departville = input('Choisir la ville de départ\n')
+    arriveville = input("Choisir la ville d'arriver\n")
+
+    # Récupération des informations (distance)
     url = "https://www.bonnesroutes.com/distance/?from="+ departville + "&to=" + arriveville
     requete = requests.get(url)
     data = BeautifulSoup(requete.content)
     # kilometres = data.find_all("td", {"class": "value"})
     # convertedInt = int(kilometres)
-    kilometres = 525
-    tempsmin = (kilometres/90)*60
-    temps = tempsmin/60
+    kilometres = 582
+
+
+    # Convertir les kilomètres en temps HH:MM
+    tempssec = (kilometres/90) * 3600
+    temps = strftime("%H : %M",localtime(tempssec))
     
+    # Resultat retourné
     print(departville, arriveville, kilometres, temps)
 
 
