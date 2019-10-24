b5a20cf5b8c9df32d6c3c3484cc0e930efd45397
Quentin Regnault <quentin.regnault@ynov.com>
2019-10-24 09:24:57 +0200

    mise en fonction

--- calcul.py
+++ calcul.py
@@ -7,12 +7,21 @@ departville = input('Choisir la ville de départ\n')
 arriveville = input("Choisir la ville d'arriver\n")
 
 
-url = "https://www.bonnesroutes.com/distance/?from="+ departville + "&to=" + arriveville
-requete = requests.get(url)
-data = BeautifulSoup(requete.content)
-# kilometres = data.find_all("td", {"class": "distance"})
-kilometres = 525
-tempsmin = (kilometres/90)*60
-temps = tempsmin/60
 
-print(departville, arriveville, kilometres, temps)
+def tempstrajet() :
+    url = "https://www.bonnesroutes.com/distance/?from="+ departville + "&to=" + arriveville
+    requete = requests.get(url)
+    data = BeautifulSoup(requete.content)
+    # kilometres = data.find_all("td", {"class": "value"})
+    # convertedInt = int(kilometres)
+    kilometres = 525
+    tempsmin = (kilometres/90)*60
+    temps = tempsmin/60
+    
+    print(departville, arriveville, kilometres, temps)
+
+
+
+tempstrajet()
+
+
